aec51bb5adb46531d787bf4ab88c6d3c6d17e3f5
jamesthomasmoran <moranja999@gmail.com>
2020-07-18 17:31:56 +0100

    spring initializer setup

---
+++ src/test/java/io/github/jamesthomasmoran/glasscuttingproblem/GlassCuttingProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jamesthomasmoran.glasscuttingproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassCuttingProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
