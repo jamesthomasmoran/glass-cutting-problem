5b00bf3c589aaeea1c54ec1ee1336686f7b9259b
jamesthomasmoran <moranja999@gmail.com>
2020-11-18 14:47:54 +0000

    pushed output state To app.js

--- glass-cutting-problem-app/src/app/App.js
+++ glass-cutting-problem-app/src/app/App.js
@@ -1,2 +1,39 @@
+import React from 'react'
+import './App.css';
+import ShapeRequestForm from './components/ShapeRequestForm/shapeRequestForm';
 
+const empty = ""
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: {
+        title: empty,
+        numberOfSheetsUsed: empty,
+        areaUsageEfficiency: empty,
+        sheets: []
+      }
+    };
+    this.setOutputState = this.setOutputState.bind(this)
+  }
+  
+setOutputState(updatedTitle, updatedSheets, updatedNumberOfSheetsUsed, updatedAreaUsageEfficiency){
+  let updatedOutput = this.state.output
+  updatedOutput.title = updatedTitle
+  updatedOutput.sheets = updatedSheets
+  updatedOutput.numberOfSheetsUsed = updatedNumberOfSheetsUsed
+  updatedOutput.areaUsageEfficiency = updatedAreaUsageEfficiency
+  this.setState({"output": updatedOutput})
+}
+
+render() {
+  return (
+    <div className="container-xl">
+        <ShapeRequestForm setOutputState={this.setOutputState}/>
+  </div>
+    )
+  }
+}
+export default App
 
